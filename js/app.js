e80929adb1b4490647d030bbc55582e39d397805
Adam Taylor Young <ayoung@berklee.edu>
2018-05-24 18:51:56 -0400

    Created variables, setUpGame() and restartGame()

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 /*
  * Create a list that holds all of your cards
  */
+const cardDeck = document.getElementById('cardDeck');
+const $deck = $('.deck');
+const deckArray = ['flask', 'paper-plane-o', 'tree', 'paw', 'cube', 'tree', 'leaf', 'bicycle', 'flask', 'bomb', 'leaf', 'bomb', 'paw', 'bicycle', 'paper-plane-o', 'cube'];
+const movesCounter = document.querySelector('.moves');
+const restartButton = document.querySelector('.restart');
+let allCards = [];
+let currentlyOpen = [];
+let moves = 0;
+let matches = 0;
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function setUpGame(){
+    allCards = shuffle(deckArray);
+    for(let i=0; i < allCards.length; i++){
+    $deck.append('<li class="card"><i class="fa fa-' + allCards[i] + '"></i></li>');
+    }
+}
+
+setUpGame();
+
+function restartGame(){
+    for(i = 0; i < 16; i++){
+        cardDeck.firstElementChild.remove();
+    }
+    moves = 0; 
+    movesCounter.textContent = moves;
+    setUpGame();
+}
+
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,15 +47,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
