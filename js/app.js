ecc0f1e4c3c0282133cfb2d7c20eb1741aa3c0b2
Adam Taylor Young <ayoung@berklee.edu>
2018-05-24 18:55:33 -0400

    Created cardsMatch and cardsDontMatch for evaluateCards function

--- js/app.js
+++ js/app.js
@@ -74,6 +74,32 @@ function evaluateCards(){
     movesCounter.textContent = moves;
 }
 
+function cardsMatch(){
+    for(i=0; i < currentlyOpen.length; i++){
+        currentlyOpen[i].classList.add('match');
+    }
+    currentlyOpen = [];
+    matches = matches + 1;
+    if(matches === 8 && moves < 10){
+        console.log('3 star performance');
+    } else if (matches === 8 && moves < 15){
+        console.log('2 star performance');
+    } else if (matches === 8 && moves < 20){
+        console.log('1 star performance')
+    } else if (matches === 8 && moves > 20) {
+        console.log('no stars for you.')
+    }
+}
+
+
+function cardsDontMatch(){
+    for(i=0; i < currentlyOpen.length; i++){
+        currentlyOpen[i].classList.toggle('open');
+        currentlyOpen[i].classList.toggle('show');
+    }
+    currentlyOpen = [];
+}
+
 cardDeck.addEventListener('click', clickShow); 
 restartButton.addEventListener('click', restartGame);
 
